63189bfd22d95c3bbd67cc6f99bd26838eff688f
Clément Théraud <theraud.clement@gmail.com>
2022-02-24 11:37:30 +0100

    card

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 import {recipes} from "./recipe.js";
-
 console.log(recipes);
+
 //Array
 let recipesArray = Object.entries(recipes);
 
-//F create recipe block
+//F create recipe block for card
 const create = (elm, attributes) => {
     const element = document.createElement(elm);
     for(let key in attributes) {
@@ -16,5 +16,19 @@ const create = (elm, attributes) => {
 let img = create("div", {class: "card-img-top card-img-placeholder", alt: "card-image"});
 let title = create("h2", {class: "card-title w-50 card-content-title"});
     title.textContent = recipes[1].name;
-let time = create("div", {class: "d-flex font-weight-bold"});
-    time.innerHTML = "<span class= 'far fa-clock mt-2' style='font-size: 1.5rem'></span>"
+    console.log(title);
+let timeParent = create("div", {class: "d-flex font-weight-bold"});
+    timeParent.innerHTML = "<span class= 'far fa-clock mt-2' style='font-size: 1.5rem'></span>"
+                    + "<p class= 'ml-2' style='font-size: 1.5rem'>" + recipes[1].time + "min </p>"
+//console.log(timeParent);
+
+    //Card Header
+    let headerParent = create("div", {class: "d-flex justify-content-between mt-3 px-3"});
+    headerParent.appendChild(title);
+    headerParent.appendChild(timeParent);
+
+    //Card Ingredients
+    let ingredients = create("div", {class: "ingredient-container"});
+
+
+    ingredients.innerHTML
