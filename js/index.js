855b07d17a732db90b09f5a437a3ddc8d9202bef
Clément Théraud <theraud.clement@gmail.com>
2022-02-28 15:15:53 +0100

    card recipe

--- js/index.js
+++ js/index.js
@@ -42,4 +42,42 @@ let createCard = (recipes) => {
         }).join("");
 
         ingredients.innerHTML = eachIngredient;
+
+        //Cook instructions
+        let instructions = create("div", {class: "description w-50"});
+        instructions.textContent = recipes[1].description;
+
+        //Device
+        let appliances = create("p", {class: "sr-only device"});
+        appliances.textContent = recipes[1].appliance;
+
+        //Utensils
+        let utensils = create("div", {class: "sr-only"});
+        let eachUtensils = recipes[1].ustensils.map(function(utensil) {
+            return "<p class= 'utensil'>" + utensil + "</p>";
+        }).join("");
+
+        utensils.innerHTML = eachUtensils;
+
+        //Card body
+        let cardBody = create("div", {class: "card-body d-flex justify-content-between card-content"});
+	
+        //combine in card body
+	    cardBody.appendChild(ingredients);
+        cardBody.appendChild(method);
+        cardBody.appendChild(appliances);
+        cardBody.appendChild(utensils);
+
+        //card container
+        let cardContainer = create("article", {class: "card recipe-card pb-3 mb-5"});
+
+        //to DOM
+        cardContainer.appendChild(image);
+        cardContainer.appendChild(headerParent);
+        cardContainer.appendChild(cardBody);
+
+        let mainSection = document.getElementById("main");
+
+        //into DOM
+        mainSection.appendChild(cardContainer);
     }
